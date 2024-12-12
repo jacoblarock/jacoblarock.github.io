50381fe90fcc935232b9ca08c2587395f3303a06
Jacob LaRock <jacob.larock@student.uni-siegen.de>
2024-12-12 09:02:27 +0100

    testing

--- navigator.js
+++ navigator.js
@@ -1,10 +1,10 @@
 function updateMain(path) {
     const main = document.getElementById("main");
-    main.innerHTML = "<h1>Hello world!</h1>";
     let home = "";
     fetch("home.html")
         .then(response => response.text())
         .then(data => home = data);
+    main.innerHTML = home;
 }
 
 updateMain("index.html");
