b44a795405dae3730e49c08004f39118f130d93b
Jacob LaRock <jacob.larock@student.uni-siegen.de>
2024-12-17 17:41:17 +0100

    framework for language changing

--- navigator.js
+++ navigator.js
@@ -8,21 +8,40 @@ async function loadPages(names) {
     return pages;
 }
 
-function updateMain(path, pages, names) {
-    let i = names.indexOf(path);
+function updateMain(path, lang, pages, names) {
+    console.log(lang);
+    let i = names.indexOf(path + "-" + lang);
     const main = document.getElementById("main");
     pages.then(data => main.innerHTML = data[i]);
 }
 
-let names = ["home", "projects", "resume", "contact"];
+function updateHeader(lang, pages, names) {
+    let i = names.indexOf("header-" + lang);
+    const header = document.getElementById("header");
+    pages.then(data => header.innerHTML = data[i]);
+}
+
+function updateLang(path, newLang, pages, names) {
+    lang = newLang;
+    updateMain(path, lang, pages, names);
+    updateHeader(lang, pages, names);
+}
+
+let names = ["home-de", "home-en", "projects-de", "projects-en", "resume-de", "resume-en", "contact-de", "contact-en", "header-de", "header-en"];
 let pages = loadPages(names);
-let split_loc = window.location.href.split("#");
-console.log(split_loc)
-let url = split_loc[0];
+let splitLoc = window.location.href.split("#");
+console.log(splitLoc);
+let url = splitLoc[0];
 let loc = "home";
-if (split_loc.length > 1)
-    loc = split_loc[1];
-console.log(loc);
+let lang = "en";
+if (splitLoc.length > 1)
+    loc = splitLoc[1];
+    splitSplitLoc = loc.split("-");
+    if (splitSplitLoc.length > 1) {
+        loc = splitSplitLoc[0];
+        lang = splitSplitLoc[1];
+    }
 document.addEventListener('DOMContentLoaded', function() {
-    updateMain(loc, pages, names);
+    updateHeader(lang, pages, names)
+    updateMain(loc, lang, pages, names);
 }, false);
